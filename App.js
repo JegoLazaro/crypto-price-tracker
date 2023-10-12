1cac44a9b5f120e7b1a1278d36ffa226896eb4ec
JegoLazaro <jose_gabriel_lazaro@dlsu.edu.ph>
2023-10-12 18:45:38 +0800

    Initial commit crypto tracker with sample data

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import ListItem from "./components/ListItem";
+
+import { SAMPLE_DATA } from './assets/data/sampleData';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.titleWrapper}>
+        <Text style={styles.largeTitle}>Markets</Text>
+        <View style={styles.divider}></View>
+        
+
+        <FlatList 
+          keyExtractor={(item) => item.id}
+          data={SAMPLE_DATA}
+          renderItem={({ item }) => (
+            <ListItem coinName={item.name} abbrv={item.symbol} priceChange={item.price_change_percentage_7d_in_currency} currentPrice={item.current_price} logo={item.image}/>
+          )}
+        />
+      </View>
     </View>
   );
 }
@@ -13,8 +28,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  largeTitle: {
+    fontSize: 34,
+    fontWeight: "bold",
+  },
+  titleWrapper: {
+    marginTop: 60,
+    paddingHorizontal: 28,
+  },
+  divider: {
+    height: .6,
+    marginTop: 10,
+    width: "98%",
+    backgroundColor: "#a9abb1",
+    justifyContent: "center",
+  }
 });
---
+++ components/ListItem.js
@@ -0,0 +1,63 @@
+import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
+import React from "react";
+
+const ListItem = ( {coinName, logo, currentPrice, priceChange, abbrv} ) => {
+    const priceChangeColor = priceChange > 0 ? "#34c759" : "#ff3b30";
+  
+    return (
+    <TouchableOpacity>
+      <View style={styles.itemWrapper}>
+        {/* Left Side */}
+        <View style={styles.leftWrapper}>
+          <Image
+            style={styles.image}
+            source={{uri: logo}}
+          />
+          <View style={styles.titleWrapper}>
+            <Text style={styles.title}>{coinName}</Text>
+            <Text style={styles.subtitle}>{abbrv.toUpperCase()}</Text>
+          </View>
+        </View>
+        {/* Right Side */}
+        <View style={styles.rightWrapper}>
+          <Text style={styles.title}>${currentPrice.toLocaleString('en-US', { currency: 'USD'})}</Text>
+          <Text style={[styles.subtitle, { color: priceChangeColor }]}>{priceChange.toFixed(2)}%</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  itemWrapper: {
+    paddingHorizontal: 16,
+    marginTop: 24,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  leftWrapper: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  image: {
+    height: 50,
+    width: 50,
+  },
+  rightWrapper: {
+    alignItems: "flex",
+  },
+  titleWrapper: {
+    marginLeft: 8,
+  },
+  title: {
+    fontSize:20,
+  },
+  subtitle: {
+    color: "#a9abb1",
+    marginTop: 4,
+    fontSize:14,
+  },
+});
+
+export default ListItem;
